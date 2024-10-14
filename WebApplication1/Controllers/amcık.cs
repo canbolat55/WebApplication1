532aa0c37e2b06377472d885bd2c936932d99f8c
canbolat55 <bugracanbolat5529@gmail.com>
2024-10-14 12:20:13 +0300

    for döng

--- WebApplication1/Controllers/amcık.cs
+++ WebApplication1/Controllers/amcık.cs
@@ -7,8 +7,8 @@ namespace WebApplication1.Controllers
         public IActionResult Index()
         {
             Console.WriteLine( "hello world");
-            for (int i = 0; i < 5; i++)
-                Console.WriteLine(i);
+          
+
             return View();
         }
     }
